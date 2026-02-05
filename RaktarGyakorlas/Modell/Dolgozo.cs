1de901604abcf7e12a1672274fabddbcc077c2a4
Nori <fubukishirou789@gmail.com>
2026-02-05 21:08:18 +0100

    Dolgozo modell kesz

--- RaktarGyakorlas/Modell/Dolgozo.cs
+++ RaktarGyakorlas/Modell/Dolgozo.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RaktarGyakorlas.Modell
 {
-    internal class Dolgozo
+    public class Dolgozo
     {
+
+        public Dolgozo() { }
+
+        public Dolgozo(string row)
+        {
+            var values = row.Split(';');
+            Id = int.Parse(values[0]);
+            Name = values[1];
+            Email = values[2];
+            Phone = values[3];
+            Position = values[4];
+        }
+        public Dolgozo(string name, string email, string phone, string position)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Position = position;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Position { get; set; } = "";
+
+        public override string? ToString()
+        {
+            return $"{Id}, Név: {Name}, Email: {Email}, Telefonszám: {Phone}, Pozíció: {Position}";
+        }
     }
 }
